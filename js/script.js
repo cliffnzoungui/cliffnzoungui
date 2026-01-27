3ce3b7feaaedea535cf3c900367eebba51426ad8
cliff NZOUNGUI <cliffnzoungui1@gmail.com>
2026-01-27 03:30:54 +0100

    Change password for opening certificate

--- js/script.js
+++ js/script.js
@@ -5,6 +5,6 @@ document.getElementById("theme-toggle").onclick=()=>{
 
 function openCertif(path){
  const pwd = prompt("Mot de passe requis :");
- if(pwd === "1234"){ window.open(path, "_blank"); }
+ if(pwd === "recrute_moi"){ window.open(path, "_blank"); }
  else{ alert("Mot de passe incorrect"); }
 }
